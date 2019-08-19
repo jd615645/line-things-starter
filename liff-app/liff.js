87ef8e669b9448762895264a7e533cfa7faf335d
ctu <ctu@cs.nctu.edu.tw>
2019-08-19 04:26:57 +0000

    :cat2: Update user_service_uuid

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'c67b4cc4-4332-4c55-bc5c-b12ef88f08e8'; // LED, Button
+const USER_SERVICE_UUID = '084f5849-c520-454c-b952-ec0ae46ef6f9'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
